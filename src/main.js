e4efc69c06c04de06f1f173a5f271c2aeaa17ac8
iv4nst <stankovicc.ivan@gmail.com>
2021-12-08 14:08:22 +0100

    Initial commit

--- src/main.js
+++ src/main.js
@@ -2,4 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+// import './assets/styles/main.css'
+import './assets/styles/style.scss'
+
+// createApp(App).use(router).mount('#app')
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
---
+++ src/views/Products.vue
@@ -0,0 +1,27 @@
+<template>
+  <main class="wrapper">
+    <h1>Products</h1>
+
+    <div class="card-container">
+      <ProductCard
+        v-for="(product, index) in inventory.slice(0, 6)"
+        :key="product.id"
+        :index="index"
+        :product="product"
+        :addToCart="addToCart"
+        class="card"
+      />
+    </div>
+  </main>
+</template>
+
+<script>
+import ProductCard from '@/components/ProductCard'
+
+export default {
+  props: ['inventory', 'addToCart'],
+  components: {
+    ProductCard
+  }
+}
+</script>
